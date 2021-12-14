f79b042ae8e2e2b0892490ba2b19329dbfffe8fb
peuks <vanmakdavid.lwg@gmail.com>
2021-12-14 20:14:18 +0100

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <h1>Happy coding with vite Reactqfdf and TS</h1>
+    </div>
+  )
+}
+
+export default App
